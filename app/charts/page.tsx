23052a51368a98c4b31294578bf4b2c1eb37bebd
Bohdan_Serhushev <bohdan_serhushev@epam.com>
2024-10-10 15:53:52 +0300

    shadcn ui test

---
+++ app/charts/page.tsx
@@ -0,0 +1,11 @@
+import { AnotherChart } from "@/components/anotherChart";
+import { ChartTry } from "@/components/chartTry";
+
+export default function CartsPage() {
+    return (
+        <>
+            <ChartTry />
+            <AnotherChart />
+        </>
+    );
+}
